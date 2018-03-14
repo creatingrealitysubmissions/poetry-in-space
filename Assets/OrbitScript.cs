bf05d4e15c0cc7fc8258602f94a98bc3e96cca91
fyhannes <higgabes@gmail.com>
2018-03-13 20:20:00 -0700

    MentorAssist build
    learned about gaze controls, simple roate script and an orbit script

---
+++ Assets/OrbitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitScript : MonoBehaviour {
+
+    public GameObject theSun;
+    public Vector3 axisToRotateAround = Vector3.up;
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(theSun.transform.position, axisToRotateAround, Time.deltaTime * speed);
+	}
+}
